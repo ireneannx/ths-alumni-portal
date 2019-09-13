7302e3991a6618e9f2a53fb21cf709b60715fa51
ireneannx <irene2104@gmail.com>
2019-09-13 09:50:48 +0530

    test. changed ports

--- backend/routes/jobs.js
+++ backend/routes/jobs.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 const db = require('../models/index')
 
+//path: /jobs 
+
 router.get('/', function (req, res, next) {
     db.Job.find()
         .then((data) => {
