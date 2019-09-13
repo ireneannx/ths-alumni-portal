f05f14630a24d7b00b3428a18ee74a749e790c96
Rituraj <riturajmandal17@outlook.com>
2019-09-13 18:44:12 +0530

    test

--- backend/models/jobschema.js
+++ backend/models/jobschema.js
@@ -1,15 +1,18 @@
 const mongoose = require('mongoose')
 
 const jobschema = new mongoose.Schema({
-    user_id :[{
-        type : String,
-        required : true
+    user_id: {
+        type: String,
+        required: true
+    },
+    job_type: String,
+    job_description: String,
+    upvote_count: [{
+        type: mongoose.Schema.Types.ObjectId,
+        unique: true
     }],
-    job_type : String,
-    job_description : String,
-    upvote_count : Number,
-    company_name : String,
-    url : String,
+    company_name: String,
+    url: String,
 })
 
 const Job = new mongoose.model("Job", jobschema)
