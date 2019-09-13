f93c35c4b68ba8bcda653eebdf8bc6d93189695e
ireneannx <irene2104@gmail.com>
2019-09-13 11:09:08 +0530

    added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Link, Route, Switch } from 'react-router-dom'
+import Navbar from './Navbar';
 import Jobs from './Jobs/Jobs'
 import AddJob from './Jobs/AddJob'
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Link to="/jobs">Jobs</Link>
 
       <Switch>
