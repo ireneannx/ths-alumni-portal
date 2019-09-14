8d0257040cf930ed73ae92d2af8333dd243c483b
rushyab <rushyab@gmail.com>
2019-09-14 19:51:15 +0530

    user and token model added

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,6 +1,11 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://rizwan-user:rizwan-user@ths-alumni-portal-g3lyi.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true })
+mongoose.connect(
+  'mongodb://localhost/alumni-test',
+  { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false }
+);
 
 module.exports.Job = require('./jobschema');
 module.exports.Posts = require('./postSchema');
+module.exports.User = require('./User');
+module.exports.Token = require('./Token');
