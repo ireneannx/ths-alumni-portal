ec37f4276ac59cdd9c9f53447549446d253db1e8
Rizwan Aman <rizwanaman5@gmail.com>
2019-09-17 18:39:16 +0530

    added auth state to Jobs, changed functional to class component for likebar

--- backend/routes/jobs.js
+++ backend/routes/jobs.js
@@ -28,36 +28,36 @@ router.get('/:jobid', authMidWare, function (req, res, next) {
 
 // to be uncommented once userschema is enabled
 
-// router.post('/:userid', authMidWare, function (req, res, next) {
-//     db.Job.create({
-//         user_id: req.params.userid,
-//         job_type: req.body.job_type,
-//         job_description: req.body.job_description,
-//         // upvote_count: 0,
-//         company_name: req.body.company_name,
-//         url: req.body.url
-//     })
-//         .then(() => {
-//             res.send(req.body)
-//         })
-//         .catch((err)=>{
-//             res.send(err)
-//         })
-// })
-
-//to be deleted once user schema is enabled
-
-router.post('/', function (req, res, next) {
+router.post('/:userid', authMidWare, function (req, res, next) {
     db.Job.create({
+        user_id: req.params.userid,
         job_type: req.body.job_type,
         job_description: req.body.job_description,
         // upvote_count: 0,
         company_name: req.body.company_name,
-        url: req.body.url,
-        deadline: req.body.deadline
+        url: req.body.url
     })
-    res.send(req.body)
+        .then(() => {
+            res.send(req.body)
+        })
+        .catch((err)=>{
+            res.send(err)
+        })
 })
+
+//to be deleted once user schema is enabled
+
+// router.post('/', function (req, res, next) {
+//     db.Job.create({
+//         job_type: req.body.job_type,
+//         job_description: req.body.job_description,
+//         // upvote_count: 0,
+//         company_name: req.body.company_name,
+//         url: req.body.url,
+//         deadline: req.body.deadline
+//     })
+//     res.send(req.body)
+// })
 
 router.put('/:jobid', authMidWare, function (req, res) {
     db.Job.findOneAndUpdate({ _id: req.params.jobid }, {
--- client/src/Jobs/likebar.js
+++ client/src/Jobs/likebar.js
@@ -1,36 +1,39 @@
 import React from 'react';
 import axios from 'axios';
 
-const LikeBar = (props) => {
+class LikeBar extends React.PureComponent {
+
     axiosCall = (e) => {
         e.preventDefault();
-        axios.post(`jobs/like/${props.upvote_count.user_id}/${props._id}`)
-    }
-    if (props.upvote_count.includes(props.upvote_count.user_id)) {
-        return (
-            <div style={{ width: "50px", margin: "0 auto", marginTop: "-26px", display: "flex" }}>
-                <div style={{ color: "red" }}>
-                    <p>&#9829;</p>
-                </div>
-                <div style={{ marginLeft: "10px", color: "white" }}>
-                    <strong>{props.upvote_count.count}</strong>
-                </div>
-            </div>
-        )
-    }
-    else {
-        return (
-            <div style={{ width: "50px", margin: "0 auto", marginTop: "-26px", display: "flex" }}>
-                <div style={{ color: "white" }}>
-                  <button onClick={()=>axiosCall}><p>&#9829;</p></button>
-                </div>
-                <div style={{ marginLeft: "10px", color: "white" }}>
-                    <strong>{props.upvote_count.count}</strong>
-                </div>
-            </div>
-        )
+        axios.post(`jobs/like/${this.props.upvote_count.user_id}/${this.props._id}`)
     }
 
+    render() {
+        if (this.props.upvote_count.includes(this.props.upvote_count.user_id)) {
+            return (
+                <div style={{ width: "50px", margin: "0 auto", marginTop: "-26px", display: "flex" }}>
+                    <div style={{ color: "red" }}>
+                        <p>&#9829;</p>
+                    </div>
+                    <div style={{ marginLeft: "10px", color: "white" }}>
+                        <strong>{this.props.upvote_count.count}</strong>
+                    </div>
+                </div>
+            )
+        }
+        else {
+            return (
+                <div style={{ width: "50px", margin: "0 auto", marginTop: "-26px", display: "flex" }}>
+                    <div style={{ color: "white" }}>
+                        <button onClick={() => this.axiosCall()}><p>&#9829;</p></button>
+                    </div>
+                    <div style={{ marginLeft: "10px", color: "white" }}>
+                        <strong>{this.props.upvote_count.count}</strong>
+                    </div>
+                </div>
+            )
+        }
+    }
 
 }
 
