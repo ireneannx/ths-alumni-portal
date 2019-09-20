09966c72888c2cd6706e1776dc15a1b269069c85
jamalmaria <jamalmaria111@gmail.com>
2019-09-20 16:31:51 +0530

    undone

--- client/src/UserProfile/JobsProfile.js
+++ client/src/UserProfile/JobsProfile.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { BrowserRouter as Router,NavLink, Route, Switch } from "react-router-dom";
+import Jobs from "../Jobs/Jobs";
+import Posts from "../Feeds/components/feed-area";
+import '../App.css'
 
 class JobsProfile extends React.PureComponent{
+    
     render(){
+        console.log("posts profile",this.props)
         return(
             <div>
                 <h1>Jobs Coming Up Soon</h1>
