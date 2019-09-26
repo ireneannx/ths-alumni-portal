b2c4d7272167c50626627353af0fef4f74592b04
Rizwan Aman <rizwanaman5@gmail.com>
2019-09-26 11:47:44 +0530

    added code-splitting and lazy loading and placeholders for start and endDate for resume forms

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import './App.css';
-import UserApp from './UserApp'
 import { Route, Switch } from 'react-router-dom';
-import SignUpForm from './Login-Signup Frontend/components/StartPage'
 import { changeAuth } from './Login-Signup Frontend/authaction'
-
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+
+// import SignUpForm from './Login-Signup Frontend/components/StartPage'
+// import UserApp from './UserApp'
+import Loading from './loading';
+
+const SignUpForm = lazy(() => import('./Login-Signup Frontend/components/StartPage'));
+const UserApp = lazy(() => import('./UserApp'));
 
 
 //auth components
@@ -17,18 +21,15 @@ function App() {
     <div>
       <React.Fragment>
 
-        <Switch>
+        <Suspense fallback={<Loading />}>
+          <Switch>
+            <Route exact path="/" component={SignUpForm} />
 
-          <Route exact path="/" component={SignUpForm} />
-        
-          <Route path="/user" component={UserApp} />
+            <Route path="/user" component={UserApp} />
+          </Switch>
+        </Suspense>
 
-          
-        </Switch>
       </React.Fragment>
-
-
-
     </div>
   );
 }
--- client/src/UserApp.js
+++ client/src/UserApp.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
-import Navbar from './Navbar';
-import Jobs from './Jobs/Jobs';
-import FeedArea from './Feeds/components/feed-area';
-import LoggedIn from './Pages/LoggedIn'
-import Profile from './UserProfile/Profile'
-import UpdateDetails from './UserProfile/CreateUser';
-import AddJob from './Jobs/AddJob';
-import Page from './ResumeBuilder/ResumeBuilder'
 
-// import history from "./history";
+import Loading from './loading';
+// import Navbar from './Navbar';
+// import Jobs from './Jobs/Jobs';
+// import FeedArea from './Feeds/components/feed-area';
+// import LoggedIn from './Pages/LoggedIn'
+// import Profile from './UserProfile/Profile'
+// import UpdateDetails from './UserProfile/CreateUser';
+// import AddJob from './Jobs/AddJob';
+// import Page from './ResumeBuilder/ResumeBuilder'
+
+const Navbar = lazy(() => import('./Navbar'));
+const Jobs = lazy(() => import('./Jobs/Jobs'));
+const FeedArea = lazy(() => import('./Feeds/components/feed-area'));
+const LoggedIn = lazy(() => import('./Pages/LoggedIn'));
+const Profile = lazy(() => import('./UserProfile/Profile'));
+const UpdateDetails = lazy(() => import('./UserProfile/CreateUser'));
+const AddJob = lazy(() => import('./Jobs/AddJob'));
+const Page = lazy(() => import('./ResumeBuilder/ResumeBuilder'));
 
 
 const UserApp = () => {
@@ -17,6 +26,7 @@ const UserApp = () => {
     <Router basename='/user'>
       <Navbar />
 
+      <Suspense fallback={<Loading />} />
       <Switch>
         <Route path="/loggedIn" exact component={LoggedIn} />
         <Route exact path="/jobs" component={Jobs} />
@@ -27,6 +37,7 @@ const UserApp = () => {
         <Route path="/resumebuilder" component={Page} />
         <Route component={<h1>sorry nothing here</h1>} />
       </Switch>
+      <Suspense />
     </Router>
   );
 }
